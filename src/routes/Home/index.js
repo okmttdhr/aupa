e1fc71413861086a46182b635070a0a39ada4364
okmttdhr <10101114fin@gmail.com>
2016-07-13 23:22:00 +0900

    add requesting

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,5 +1,19 @@
 import HomeView from './components/HomeView'
 
+import request from 'superagent'
+import utils from 'utils'
+
+request
+  .get(utils.uriSearch().youtube)
+  .query({
+    part: 'snippet',
+    key: utils.apiKey().youtube,
+  })
+  .end((err, res) => {
+    console.log(err)
+    console.log(res)
+  })
+
 // Sync route definition
 export default {
   component: HomeView
---
+++ src/utils/config.js
@@ -0,0 +1,28 @@
+export function apiKey() {
+  return {
+    giphy: 'dc6zaTOxFJmzC',
+  }
+}
+
+export function uriSearch() {
+  let host = ''
+  const productionHost = {
+    youtube: 'https://www.googleapis.com/youtube/v3/search',
+    giphy: 'http://api.giphy.com/v1/gifs/search',
+  }
+
+  switch (process.env.NODE_ENV) {
+    case 'e2eTest':
+    case 'development':
+    case 'production':
+    default:
+      host = productionHost
+      break
+  }
+
+  return host
+}
+
+export function errorMessage() {
+  return
+}
