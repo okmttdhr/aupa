c1f168aa8a96435503afdc9ce02b2d3e8e3a6ca1
okmttdhr <10101114fin@gmail.com>
2016-07-21 21:57:32 +0900

    remove iframe inline stuff

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,18 +1,6 @@
 import React from 'react'
 import classes from './style.scss'
 
-import makeVideoPlayableInline from 'iphone-inline-video'
-
-window.onload = () => {
-  setTimeout(() => {
-    const iframe = document.querySelectorAll('iframe')[4]
-    const video = iframe.contentWindow.document.querySelector('video')
-    console.log(video)
-    video.setAttribute('webkit-playsinline')
-    makeVideoPlayableInline(video)
-  }, 5000)
-}
-
 export const Video = (props) => (
   <div className={classes.Video}>
     <iframe width='420' height='345' frameBorder='0' src={props.embedSrc} />
